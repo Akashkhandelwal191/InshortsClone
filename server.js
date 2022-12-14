b371a837af035bc4474bf73327889f891f3be9fa
Akash khandelwal <skykhandelwal1999@gmail.com>
2022-12-15 01:58:44 +0530

    All New Configuration Done

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 import express from 'express';
+import * as url from 'url';
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+// const express = require('express');
 // import Connection from './database/db.js';
 // import DefaultData from './default.js';
 import Route from './routes/route.js';
+// const Route = require('./routes/route.js');
 import cors from 'cors';
+// const cors = require('cors');
 import dotenv from 'dotenv';
+// const dotenv = require('dotenv');
+import path from 'path';
+// const path = require('path');
 
 const app = express();
 dotenv.config();
@@ -15,12 +24,20 @@ const PORT = process.env.PORT || 8000;
 
 // const username = process.env.DB_USERNAME;
 // const password = process.env.DB_PASSWORD;
-if(process.env.NODE_ENV === 'production')
-{
-     app.use(express.static("client/build"));
-}
+// if(process.env.NODE_ENV === 'production')
+// {
+//      app.use(express.static("client/build"));
+// }
 
-
+app.use(express.static(path.join(__dirname, "./client/build")));
+app.get("*", function (_, res) {
+  res.sendFile(
+    path.join(__dirname, "./client/build/index.html"),
+    function (err) {
+      res.status(500).send(err);
+    }
+  );
+});
 
 
 app.listen(PORT,()=>{
